0f3686d63808e794885aac2f4a04831ef97fbe3e
Michael Rommel <michael.rommel@mobility-media.de>
2016-12-13 12:59:09 +0100

    add labels to detail view

--- MiRoRecipeBook/MiRoRecipeBook/Presentation/RecipeDetailViewController.swift
+++ MiRoRecipeBook/MiRoRecipeBook/Presentation/RecipeDetailViewController.swift
@@ -15,6 +15,10 @@ class RecipeDetailViewController: UIViewController {
     
     @IBOutlet weak private var scrollView: UIScrollView!
     @IBOutlet weak private var stackView: UIStackView!
+    
+    @IBOutlet weak private var imageLabel: UIImageView!
+    @IBOutlet weak private var titleLabel: UILabel!
+    @IBOutlet weak private var descLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,92 +31,9 @@ class RecipeDetailViewController: UIViewController {
         if recipe != nil {
             NSLog("recipe: %@", recipe ?? "<default>")
             self.title = recipe?.name
-            //self.descLabel?.text = (recipe!.teaser)! + " " + (recipe!.desc)!;
-            //self.imageLabel?.setImage(withUrl: recipe!.getImageUrl()!, placeholder: UIImage(named: "recipe-default-image.png"))
-            
-            addEntry(sender: self)
+            self.titleLabel?.text = recipe?.name
+            self.descLabel?.text = (recipe!.teaser)! + " " + (recipe!.desc)!;
+            self.imageLabel?.setImage(withUrl: recipe!.getImageUrl()!, placeholder: UIImage(named: "recipe-default-image.png"))
         }
     }
-    
-    @IBAction func addEntry(sender: AnyObject) {
-        
-        let stack = stackView
-        var index = (stack?.arrangedSubviews.count)! - 1
-        
-        var h: CGFloat = 0.0
-        if index != -1 {
-            let addView = stack?.arrangedSubviews[index]
-            h = (addView?.frame.size.height)!
-        } else {
-            index = 0
-        }
-        
-        
-        let scroll = scrollView
-        let offset = CGPoint(x: (scroll?.contentOffset.x)!,
-                             y: (scroll?.contentOffset.y)! + h)
-        
-        let newView = createEntry()
-        newView.isHidden = true
-        stack?.insertArrangedSubview(newView, at: index)
-        
-        UIView.animate(withDuration: 0.25) { () -> Void in
-            newView.isHidden = false
-            scroll?.contentOffset = offset
-        }
-    }
-    
-    @IBAction func goBackToList(_ sender: AnyObject) {
-        dismiss(animated: true, completion: nil)
-    }
-    
-    // MARK: - Private Methods
-    private func createEntry() -> UIView {
-        let date = DateFormatter.localizedString(from: NSDate() as Date, dateStyle: .short, timeStyle: .none)
-        let number = "\(randomHexQuad())-\(randomHexQuad())-\(randomHexQuad())-\(randomHexQuad())"
-        
-        let stack = UIStackView()
-        stack.axis = .horizontal
-        stack.alignment = .firstBaseline
-        stack.distribution = .fill
-        stack.spacing = 8
-        
-        let dateLabel = UILabel()
-        dateLabel.text = date
-        dateLabel.font = UIFont.preferredFont(forTextStyle: UIFontTextStyle.body)
-        
-        let numberLabel = UILabel()
-        numberLabel.text = number
-        numberLabel.font = UIFont.preferredFont(forTextStyle: UIFontTextStyle.headline)
-        
-        let deleteButton = UIButton(type: .roundedRect)
-        deleteButton.setTitle("Delete", for: .normal)
-        deleteButton.addTarget(self, action: Selector("deleteStackView"), for: .touchUpInside)
-        
-        stack.addArrangedSubview(dateLabel)
-        stack.addArrangedSubview(numberLabel)
-        stack.addArrangedSubview(deleteButton)
-        
-        return stack
-    }
-    
-    func deleteStackView(sender: UIButton) {
-        /*if let view = sender.superview {
-            UIView.animateWithDuration(0.25, animations: { () -> Void in
-                view.hidden = true
-            }, completion: { (success) -> Void in
-                view.removeFromSuperview()
-            })
-        }*/
-    }
-    
-    private func randomHexQuad() -> String {
-        return NSString(format: "%X%X%X%X",
-                        arc4random() % 16,
-                        arc4random() % 16,
-                        arc4random() % 16,
-                        arc4random() % 16
-            ) as String
-    }
-
 }
