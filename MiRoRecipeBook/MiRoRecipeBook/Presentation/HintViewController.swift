eb709996507dd7a799bb5f16fbc705959490ffa5
Michael Rommel <michael.rommel@mobility-media.de>
2016-12-13 15:46:20 +0100

    add animation to explore the app

--- MiRoRecipeBook/MiRoRecipeBook/Presentation/HintViewController.swift
+++ MiRoRecipeBook/MiRoRecipeBook/Presentation/HintViewController.swift
@@ -11,15 +11,33 @@ import UIKit
 class HintViewController: UIViewController {
     
     @IBOutlet weak var hintLabel: UILabel!
+    @IBOutlet weak var exploreImage: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.title = "hint.title".localized
         self.hintLabel.text = "hint.message".localized
+        
+        UIView.animate(withDuration: 1.0, delay: 3.0, options: .curveEaseOut, animations: {
+            // Here you will get the animation you want
+            self.exploreImage.alpha = 1
+        }, completion: { finished in
+            // Here you hide it when animation done
+            self.exploreImage.isHidden = false
+        })
     }
     
     @IBAction func openMenu(_ sender: AnyObject) {
+        
+        UIView.animate(withDuration: 1.0, delay: 1.0, options: .curveEaseOut, animations: {
+            // Here you will get the animation you want
+            self.exploreImage.alpha = 0
+        }, completion: { finished in
+            // Here you hide it when animation done
+            self.exploreImage.isHidden = true
+        })
+        
         self.revealViewController().revealToggle(nil)
     }
 }
